a06c78851d1b35432438dd29848e6ad00d641709
prashantsagar73 <prashant.sagar73@gmail.com>
2021-12-10 21:52:33 +0530

    form in react

---
+++ first/src/Forms.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Forms=()=>{
+    const [name, setname]= useState(" ");
+    const [FullName, setFullname] = useState(" ");
+
+    const InputEvent=(events)=> {
+        // console.log(events.target.value);
+        setname(events.target.value);
+    };
+
+    const submit=()=>{
+        setFullname(name);
+    };
+
+    return(
+        <>
+        <div>
+        <h4> hello { FullName}</h4>
+        <input type="text" placeholder="Enter your name" onChange={InputEvent} value={name} />
+        <button onClick={submit}> Click Me</button>
+        </div>
+        </>
+    );
+}
+
+export default Forms;
--- first/src/index.js
+++ first/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Events from './Events';
+import Forms from './Forms';
 
 
 ReactDOM.render(
   <>
-  <Events />
+  <Forms />
 </>,
   
   document.getElementById('root')
